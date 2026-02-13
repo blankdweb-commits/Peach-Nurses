053b2f5fc13780da85f1b5a109b23dd8a4476402
blankdweb-commit <blankdweb@gmail.com>
2026-02-13 13:21:59 -0800

    update

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo: React.FC = () => {
+  return (
+    <div className="text-center mb-8">
+      <span className="text-6xl mb-4 block">🍑</span>
+      <h1 className="text-4xl font-black text-gray-800">Peach</h1>
+    </div>
+  );
+};
+
+export default Logo;
